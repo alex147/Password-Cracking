e8c2e9dc819ccaadc44e07d4f3f078da0b06ab8b
Admin <Admin@10.153.2.24>
2013-04-05 13:44:28 +0200

    Added a new double variable.

---
+++ src/java/Wrapper/Wrapper.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Wrapper;
+
+import java.util.List;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Admin
+ */
+@WebService(serviceName = "Wrapper")
+public class Wrapper {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+//    @WebMethod(operationName = "getDictionaryPart")
+//    public List<String> getDictionaryPart(){
+//       return list; 
+//    }
+//    @WebMethod(operationName = "getPasswordFile")
+//    public List<String> getPasswordFile(){
+//        return list;
+//    }
+    @WebMethod(operationName = "sendCracked")
+    public void sendCracked(List<String> list){
+        
+    }
+}
