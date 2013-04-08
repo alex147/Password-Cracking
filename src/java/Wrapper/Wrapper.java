2a74e03531ac49ae6f1ed82367fbb38e0331d2f3
Admin <Admin@10.153.2.24>
2013-04-08 08:51:05 +0200

    Added a new double variable.

--- src/java/Wrapper/Wrapper.java
+++ src/java/Wrapper/Wrapper.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 
