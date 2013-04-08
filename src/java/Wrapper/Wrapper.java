6c0161e8f645d6e52aa27653186bd78f662df669
Admin <Admin@10.153.2.24>
2013-04-08 07:43:35 +0200

    Added a new double variable.

--- src/java/Wrapper/Wrapper.java
+++ src/java/Wrapper/Wrapper.java
@@ -7,6 +7,9 @@ package Wrapper;
 import Master.*;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 
@@ -20,18 +23,30 @@ public class Wrapper {
     private static int usersNumber = 4;
     private Master master = Master.getInstance();
     private PassFileOpener passFileOpener = PassFileOpener.getInstance();
+    private static final Logger logger = Logger.getLogger("DPCLogger");
+    private FileHandler handler;
 
     @WebMethod(operationName = "getDictionaryPart")
-    public List<String> getDictionaryPart() throws IOException{
+    public List<String> getDictionaryPart() throws IOException {
         return master.getDictionary(usersNumber);
     }
 
     @WebMethod(operationName = "getPasswordFile")
-    public List<String> getPasswordFile() throws IOException{
+    public List<String> getPasswordFile() throws IOException {
         return passFileOpener.getPassFile();
     }
 
     @WebMethod(operationName = "sendCracked")
-    public void sendCracked(List<String> list) {
+    public void sendCracked(List<String> list) throws IOException {
+        setLogger();
+        for(int i = 0; i<list.size(); i++){
+        logger.info(list.get(i));
+        }
+    }
+
+    private void setLogger() throws IOException {
+        handler = new FileHandler("C:\\DPC_Log.txt");
+        handler.setFormatter(new SimpleFormatter());
+        logger.addHandler(handler);
     }
 }
