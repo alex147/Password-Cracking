bf42e03d8806dcd72cc02149e197be04321a8db3
Admin <Admin@10.153.2.24>
2013-04-08 07:25:38 +0200

    Added a new double variable.

--- src/java/Master/Master.java
+++ src/java/Master/Master.java
@@ -1,7 +1,6 @@
 package Master;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -28,33 +27,27 @@ public class Master {
     public List<String> getDictionary(int usersNumber) throws IOException {
         List<String> dictionaryList = new LinkedList<>();
         timesCalled++;
-//        if (timesCalled == 1) {
-                FileReader fileReader = new FileReader("C:\\webster-dictionary.txt");
-                bufferedReader = new BufferedReader(fileReader);
-//            openDictionaryFile();
-//        }
-//        else {
-//            bufferedReader.reset();
-//        }
+        if (timesCalled == 1) {
+            openDictionaryFile();
+        }
+        else {
+            bufferedReader.reset();
+        }
         String line;
-        for (int i = 0; i < linesNumber / usersNumber; i++) {
+        for (int i = 0; i < (linesNumber / usersNumber) / 500; i++) {
             line = bufferedReader.readLine();
             if (line == null) {
                 break;
             } else {
                 dictionaryList.add(line);
             }
-//            bufferedReader.mark(1);
+            bufferedReader.mark(1);
         }
         return dictionaryList;
     }
-//    private void openDictionaryFile() throws IOException {
-//        FileReader fileReader;
-//        try{
-//            fileReader = new FileReader("C:\\webster-dictionary.txt");
-//            bufferedReader = new BufferedReader(fileReader);
-//        }catch (IOException exception){
-//            System.err.println(exception.getMessage());
-//        }
-//    }
+
+    private void openDictionaryFile() throws IOException {
+        FileReader fileReader = new FileReader("C:\\webster-dictionary.txt");
+        bufferedReader = new BufferedReader(fileReader);
+    }
 }
